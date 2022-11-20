64d8bda6d9f85ebb7c61f0f569fc41d468c6f157
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-11-20 19:53:52 +0100

    feat: create project

---
+++ server/api/server/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/SolomonRosemite/Mixify/api/schema"
+	"github.com/SolomonRosemite/Mixify/api/schema/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &schema.Resolver{}}))
+
+	http.Handle("/playground", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/playground for GraphQL playground", port)
+
+	// When running in docker this might have to be 0.0.0.0:port or just :port instead of localhost:port
+	srvUrl := "localhost:" + port
+	log.Fatal(http.ListenAndServe(srvUrl, nil))
+}
