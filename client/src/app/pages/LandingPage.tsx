19d30de6faa2842fb6a5b0638f941d16c0439fad
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-12-25 12:45:53 +0100

    refactor: change landing page input using daisyui

--- client/src/app/pages/LandingPage.tsx
+++ client/src/app/pages/LandingPage.tsx
@@ -6,16 +6,18 @@ const LandingPage: Component = () => {
       <div>
         <div class="bg-red-300 p-5">
           <h1 class="text-4xl text-center">Login to Mixify</h1>
-          <div class="flex justify-center">
-            <div class="mx-1 my-2">
-              <label class="block">Email</label>
-              <input class="rounded p-2" type="text" />
+          <div class="flex flex-col items-center">
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Your Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="example@mail.com"
+                class="input input-bordered w-full max-w-xs"
+              />
             </div>
-          </div>
-          <div class="flex justify-center">
-            <button class="bg-black text-white rounded-md px-5 py-2 outline-none border-none">
-              Sign in
-            </button>
+            <button class="btn my-2">Sign in</button>
           </div>
         </div>
       </div>
