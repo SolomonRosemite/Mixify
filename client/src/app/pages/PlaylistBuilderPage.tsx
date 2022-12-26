a6de7619c874799a917fb9127ffc209c05a8c0c0
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-12-26 13:29:40 +0100

    feat: add playlist builder page

---
+++ client/src/app/pages/PlaylistBuilderPage.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+
+const PlaylistBuilderPage: Component = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <button class="btn btn-error">Sign out</button>
+    </div>
+  );
+};
+
+export default PlaylistBuilderPage;
