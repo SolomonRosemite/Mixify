a353b62a8c8f95273f2b734f39319c49a045e979
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-12-26 13:29:40 +0100

    feat: add playlist builder page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import type { Component } from "solid-js";
 import EmailConfirmationPage from "./app/pages/EmailConfirmationPage";
 import LandingPage from "./app/pages/LandingPage";
+import PlaylistBuilderPage from "./app/pages/PlaylistBuilderPage";
 
 // const App: Component = () => <LandingPage />;
-const App: Component = () => <EmailConfirmationPage />;
+// const App: Component = () => <EmailConfirmationPage />;
+const App: Component = () => <PlaylistBuilderPage />;
 
 export default App;
