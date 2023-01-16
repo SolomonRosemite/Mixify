a17a71f716ba82bc178963ebdb7a5ee82e570467
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-01-16 22:55:43 +0100

    feat: add queries to fetch user access token & configuration

--- server/api/graphql/schema.resolvers.go
+++ server/api/graphql/schema.resolvers.go
@@ -8,9 +8,11 @@ import (
 
 	"github.com/SolomonRosemite/Mixify/api/graphql/generated"
 	"github.com/SolomonRosemite/Mixify/api/graphql/model"
+	configurations "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/Configurations"
 	confirmConfirmationCode "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/ConfirmConfirmationCode"
 	createPlaylistSnapshotConfiguration "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/CreatePlaylistSnapshotConfiguration"
 	createSyncPlaylistsEvent "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/CreateSyncPlaylistsEvent"
+	requestAccessToken "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/RequestAccessToken"
 	requestConfirmationCode "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/RequestConfirmationCode"
 	syncEvents "github.com/SolomonRosemite/Mixify/api/graphql/resolvers/SyncEvents"
 )
@@ -26,19 +28,30 @@ func (r *mutationResolver) CreatePlaylistSnapshotConfiguration(ctx context.Conte
 	return createPlaylistSnapshotConfiguration.CreatePlaylistSnapshotConfiguration(&ctx, &input, r.DB.DB)
 }
 
+// RequestAccessToken is the resolver for the requestAccessToken field.
+func (r *queryResolver) RequestAccessToken(ctx context.Context) (*model.RequestAccessTokenResponse, error) {
+	// TODO: Remove test user in the future
+	return requestAccessToken.RequestAccessToken(ctx, (*r.SpotifyUserAccess)["user_id:1"])
+}
+
 // RequestConfirmationCode is the resolver for the requestConfirmationCode field.
 func (r *queryResolver) RequestConfirmationCode(ctx context.Context, email string) (*model.RequestConfirmationCodeResponse, error) {
 	return requestConfirmationCode.RequestConfirmationCode(ctx, email, r.EmailConfirmationCodes)
 }
 
 // ConfirmConfirmationCode is the resolver for the confirmConfirmationCode field.
-func (r *queryResolver) ConfirmConfirmationCode(ctx context.Context, confirmationCode string, confirmationSecret *string) (*model.User, error) {
-	return confirmConfirmationCode.ConfirmConfirmationCode(ctx, confirmationCode, *confirmationSecret, r.EmailConfirmationCodes, r.DB.DB)
+func (r *queryResolver) ConfirmConfirmationCode(ctx context.Context, confirmationCode string, confirmationSecret string) (*model.User, error) {
+	return confirmConfirmationCode.ConfirmConfirmationCode(ctx, confirmationCode, confirmationSecret, r.EmailConfirmationCodes, r.DB.DB)
 }
 
 // SyncEvents is the resolver for the syncEvents field.
 func (r *queryResolver) SyncEvents(ctx context.Context, id string) (*model.SyncPlaylistsEvent, error) {
 	return syncEvents.SyncEvents(ctx, id, r.DB.DB)
+}
+
+// Configurations is the resolver for the configurations field.
+func (r *queryResolver) Configurations(ctx context.Context, id string) (*model.PlaylistSnapshotConfiguration, error) {
+	return configurations.Configurations(ctx, id, r.DB.DB)
 }
 
 // Mutation returns generated.MutationResolver implementation.
