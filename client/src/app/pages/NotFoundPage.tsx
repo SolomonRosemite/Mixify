412cc2bb47ad393a2aaf8a6f3eff379ef87ef6f4
SolomonRosemite <solomonrosemiteprojects@gmail.com>
2023-05-09 15:03:56 +0200

    feat: remove old project
    after losing my sanity using go and gorm its time to move on

--- client/src/app/pages/NotFoundPage.tsx
+++
@@ -1,8 +0,0 @@
-import type { Component } from "solid-js";
-
-const NotFoundPage: Component = () => {
-  // TODO: Finish page
-  return <div>Okay, this is not good</div>;
-};
-
-export default NotFoundPage;
