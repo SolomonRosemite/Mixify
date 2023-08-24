0b313f0a28806cb3635a7d430462694e7a840060
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-08-24 11:50:26 +0200

    feat: add suport api stuff and test code

---
+++ src/src/apply_command.rs
@@ -0,0 +1,10 @@
+use super::args;
+
+pub fn handle_apply_snapshot(cmd: &args::ApplyCommand) {
+    let path = format!("snapshots/{}/1_init.edit.gv", cmd.id);
+    // let x = std::fs::read_dir(folder).unwrap().collect::<Vec<_>>();
+    let content = std::fs::read_to_string(path).unwrap();
+    let gv = graphviz_dot_parser::parse(&content).unwrap();
+
+    println!("{:?}", gv.stmt);
+}
