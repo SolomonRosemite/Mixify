4f5f567dae0b0f0f1a47e9b03b3aa349f1d2c29c
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-08-25 14:25:19 +0200

    feat: resolve regarding to having multiple roots

--- src/src/constants.rs
+++ src/src/constants.rs
@@ -1,4 +1,6 @@
 pub const SNAPSHOTS_FOLDER: &str = "snapshots";
 
+pub const MIXIFY_TEMPORARY_ROOT_NODE_NAME: &str = "__mixify_root";
+
 pub const SUBTRACT_ATTRIBUTE_KEY: &str = "subtract";
 pub const URL_ATTRIBUTE_KEY: &str = "URL";
