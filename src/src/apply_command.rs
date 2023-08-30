3ec8f17744591673bf75b64f9579b09be0985b24
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-08-30 15:57:08 +0200

    refactor: more plan and apply changes

--- src/src/apply_command.rs
+++ src/src/apply_command.rs
@@ -1,11 +1,10 @@
+use crate::plan_command;
+
 use super::args;
 
 pub fn handle_apply_snapshot(cmd: &args::ApplyCommand) -> Result<(), anyhow::Error> {
-    let path = format!("snapshots/{}/1_init.edit.gv", cmd.id);
-    // let x = std::fs::read_dir(folder).unwrap().collect::<Vec<_>>();
-    let content = std::fs::read_to_string(path).unwrap();
-    let gv = graphviz_dot_parser::parse(&content).unwrap();
+    let gv = plan_command::read_snapshot(cmd.id, "edit")?;
+    let res = plan_command::create_execution_plan(&gv)?;
 
-    println!("{:?}", gv.stmt);
     return Ok(());
 }
