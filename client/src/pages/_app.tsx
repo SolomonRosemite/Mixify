c2d6e0f962fdb0863255a535814dade922bb712e
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2023-10-11 10:23:20 +0200

    feat: add react flow

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,7 +2,8 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
-import "~/styles/globals.css";
+// import "~/styles/globals.css";
+import "~/styles/react-flow.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
